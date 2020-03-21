47038030972d0890716136ab438b4c3c40041611
Sang-gon Lee <lee.sg16@gmail.com>
2020-03-21 10:59:58 -0400

    omit entityId from file path

--- services/storage_service.js
+++ services/storage_service.js
@@ -1,16 +1,9 @@
 import * as s3Manager from '../managers/s3_manager';
 
-export async function uploadImageFile({
-  entityType,
-  entityId,
-  relation,
-  contentType,
-  imageExtension,
-  file,
-}) {
+export async function uploadImageFile({ entityType, relation, contentType, imageExtension, file }) {
   // Use upload time for file name
   const filename = `${new Date().toISOString()}.${imageExtension}`;
-  const key = `${entityType}/${entityId}/${relation}/${filename}`;
+  const key = `${entityType}/${relation}/${filename}`;
   return await s3Manager.upload({
     key,
     contentType,
